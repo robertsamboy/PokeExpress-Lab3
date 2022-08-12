25d7e0d40d868845025c45da4ea7f089e8202a2a
robertsamboy <106219442+robertsamboy@users.noreply.github.com>
2022-08-12 13:28:23 -0400

    yooo

--- pokemon_app/views/Intro.jsx
+++ pokemon_app/views/Intro.jsx
@@ -13,7 +13,7 @@ class Intro extends React.Component{
             <body style = {design}>
                 <div>
             <h1>WELCOME TO THE POKELIBRARY</h1>
-            <nav><h2><a href = {`/pokemon/${pokemon.id}`}>Click here to start</a></h2></nav>
+            <nav><h2><a href = {`/pokemon/`}>Click here to start</a></h2></nav>
             </div>
             </body>
 
