00726737a45c3f3d3caf51bbc45a4dc59cd11992
robertsamboy <106219442+robertsamboy@users.noreply.github.com>
2022-08-12 13:13:25 -0400

    DEPLOYING APP

--- pokemon_app/views/Intro.jsx
+++ pokemon_app/views/Intro.jsx
@@ -8,7 +8,13 @@ class Intro extends React.Component{
             backgroundColor: 'blue'
         }
         return(
+            <body style = {design}>
+                <div>
             <h1>WELCOME TO THE POKELIBRARY</h1>
+            <nav><h2><a href = "/pokemon/">Click here to start</a></h2></nav>
+            </div>
+            </body>
+
         )
 
         
