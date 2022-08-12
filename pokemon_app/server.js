ca69055b66050273b93030beadc8b3b720b04596
robertsamboy <106219442+robertsamboy@users.noreply.github.com>
2022-08-12 13:03:08 -0400

    Deploying App

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -19,9 +19,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
 
-app.get('/',(req,res)=>{
-    res.send('Welcome to the Pokemon App!')
-})
+
 
 // app.get('/pokemon/',(req,res)=>{
 //         res.render('Index', {pokemons:pokemons})
